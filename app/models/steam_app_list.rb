feec3be49c1268f07d3872b4cc7e8a065073b0ef
andy tham <thamandyy@gmail.com>
2018-02-11 01:01:11 -0500

    search prep

---
+++ app/models/steam_app_list.rb
@@ -0,0 +1,2 @@
+class SteamAppList < ApplicationRecord
+end
---
+++ test/models/steam_app_list_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class SteamAppListTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
