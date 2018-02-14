e6f9e4e714784134b67fad1e74fa05698569aa52
andy tham <thamandyy@gmail.com>
2018-02-14 02:04:51 -0500

    more css

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,5 @@
 class HomeController < ApplicationController
   def index
-    p "hello world"
-    @string = "test string"
-    response = RestClient::Request.execute(
-      method: :get,
-      url: 'https://accesscontrolalloworiginall.herokuapp.com/https://api.steampowered.com/ISteamNews/GetNewsForApp/v2/?appid=440&count=3\r\n'
-    )
-    @data = JSON.parse(response)["appnews"]["newsitems"][0]["contents"]
   end
 
 end
