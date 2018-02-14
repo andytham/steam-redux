69325869fee5dd5244039606525f56da95f47c56
andy tham <thamandyy@gmail.com>
2018-02-13 23:10:50 -0500

    fixed 0 news count

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -11,15 +11,17 @@ class NewsController < ApplicationController
         url: "https://accesscontrolalloworiginall.herokuapp.com/https://api.steampowered.com/ISteamNews/GetNewsForApp/v2/?appid=#{app.appid}&count=3\r\n"
       )
       poop = JSON.parse(response)
-
-      if poop != {}
+      if poop != {} && poop["appnews"]["count"] != 0
         @newsObj[:name] = app.name
         @newsObj[:content] = JSON.parse(response)["appnews"]["newsitems"][0]["contents"]
-        @newsAll << @newsObj
-        puts @newsObj
+        # need to shallow copy with clone or mutating the obj will mess with the array's clone as it points to the object
+        @newsAll << @newsObj.clone
+        @newsObj.clear
+
       end
 
     end
+
   end
 
 end
