81f00dee6f0ad2e557a7192dfd516ea6c7468b0f
andy tham <thamandyy@gmail.com>
2018-03-04 19:07:33 -0500

    heroku

--- app/models/steam_app.rb
+++ app/models/steam_app.rb
@@ -1,6 +1,6 @@
 class SteamApp < ApplicationRecord
   attr_accessor :name, :appid
-  belongs_to :user
+  belongs_to :user, optional: true
   scoped_search on: [:name, :appid]
   # default_operator overrides LIKE
   scoped_search on: :appid, profile: :appid, default_operator: :eq,complete_value: true
